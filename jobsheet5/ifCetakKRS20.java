8298241d2bff8958ab7cbd088d6535e395403a9b
Nulsifr-dev <rifqiabdurrahman10@gmail.com>
2025-10-05 22:17:32 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS20.java
+++ jobsheet5/ifCetakKRS20.java
@@ -9,9 +9,18 @@ public class ifCetakKRS20 {
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktlunas = mra.nextBoolean();
 
-        if (uktlunas){
+         if (uktlunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Pembayaran UKT belum lunas, silahkan melakukan pembayaran terlebih dahulu");
         }
+        
+        System.out.println("\n=== Versi Ternary Operator ===");
+        String pesan = (uktlunas)
+                ? "Pembayaran UKT terverifikasi.\nSilahkan cetak KRS dan minta tanda tangan DPA."
+                : "Pembayaran UKT belum lunas, silahkan melakukan pembayaran terlebih dahulu.";
+
+        System.out.println(pesan);
     }
 }
