4a30e4b55b231a1dea6d4690145cfd27543a3445
Nulsifr-dev <rifqiabdurrahman10@gmail.com>
2025-10-09 09:31:44 +0700

    gagal

--- jobsheet5/KelulusanSemesterMRA20.java
+++
@@ -1,9 +0,0 @@
-package jobsheet5;
-import java.util.Scanner;
-
-public class KelulusanSemesterMRA20 {
-    Scanner mra = new Scanner(System.in);
-
-    System.out.println("===== INPUT DATA MAHASISWA =====");
-    
-}
