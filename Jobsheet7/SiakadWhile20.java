aeb393026a49be8f3844616a5afad7ea7363808a
Nulsifr-dev <rifqiabdurrahman10@gmail.com>
2025-10-23 11:36:00 +0700

    SiakadWhile20 Belum selesai

---
+++ Jobsheet7/SiakadWhile20.java
@@ -0,0 +1,14 @@
+package Jobsheet7;
+import java.util.Scanner;
+
+public class SiakadWhile20 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int nilai, jml, i = 0;
+        System.out.print("masukkan jumlah mahasiswa: ");
+        jml=sc.nextInt();
+    }
+    
+}
