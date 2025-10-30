c8066570ed92f8c405feac00a01b435143382b02
Nulsifr-dev <rifqiabdurrahman10@gmail.com>
2025-10-30 10:41:45 +0700

    masih progress (jobsheet 8 T1)

---
+++ Jobsheet8/JumlahKuadratMRA20.java
@@ -0,0 +1,12 @@
+package Jobsheet8;
+
+import java.util.Scanner;
+
+public class JumlahKuadratMRA20 {
+    public static void main(String[] args) {
+        Scanner mra = new Scanner(System.in);
+
+        
+    }
+    
+}
