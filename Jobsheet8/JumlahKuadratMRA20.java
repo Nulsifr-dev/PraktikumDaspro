acf083c4f3d18555ec47f739b6409e2333ed57b1
Nulsifr-dev <rifqiabdurrahman10@gmail.com>
2025-11-01 21:13:03 +0700

    Tugas1 Jobsheet 8

--- Jobsheet8/JumlahKuadratMRA20.java
+++ Jobsheet8/JumlahKuadratMRA20.java
@@ -6,7 +6,26 @@ public class JumlahKuadratMRA20 {
     public static void main(String[] args) {
         Scanner mra = new Scanner(System.in);
 
-        
+        System.out.print("masukkan nilai n: ");
+        int n = mra.nextInt();
+
+        int i, j, kuadrat = 0;
+
+        for (j = 1; j <= n; j++) {
+            int jumlah = 0;
+            System.out.print("n = " + j + ">> Jumlah kuadrat = ");
+            for (i = 1; i <= j; i++){
+                kuadrat = i*i;
+                jumlah += kuadrat;
+                
+                System.out.print(kuadrat);
+                if (i<j){
+                    System.out.print(" + ");
+                }
+            }
+            System.out.println(" = " + jumlah);
+        }
+        mra.close();
     }
     
 }
