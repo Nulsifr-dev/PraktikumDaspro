a10daf10b0b5be43f3fc1e7264c56863e3f54d09
Nulsifr-dev <rifqiabdurrahman10@gmail.com>
2025-11-27 09:34:44 +0700

    Pertemuan14-Jobsheet12-P2

--- Pertemuan14/Kafe19.java
+++ Pertemuan14/Kafe19.java
@@ -2,7 +2,13 @@ package Pertemuan14;
 
 public class Kafe19 {
 
-    public static void Menu() {
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, "+namaPelanggan+"!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam  Rp 15,000");
         System.out.println("2. Capuccino - Rp 20,000");
@@ -14,7 +20,7 @@ public class Kafe19 {
         System.out.println("Silakan pilih menu yang Anda inginkan.");
     }
     public static void main(String[] args) {
-        Menu();
+        Menu("Andi", true);
     }
     
 }
