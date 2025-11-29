7ef288f24778651a0182fde5c1aca83ea62469da
Nulsifr-dev <rifqiabdurrahman10@gmail.com>
2025-11-29 21:27:14 +0700

    Percobaan1-Pertemuan14-Jobsheet12

--- Pertemuan14/Kafe19.java
+++ Pertemuan14/Kafe19.java
@@ -1,20 +1,20 @@
 package Pertemuan14;
 
 public class Kafe19 {
-    public static void menu() {
-        System.out.println("==== MENU RESTO KAFE ====");
+
+    public static void Menu() {
+        System.out.println("===== MENU KAFE RESTO =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Capuccino - Rp 20,000");
         System.out.println("3. Latte - Rp 22,000");
         System.out.println("4. Teh Tarik - Rp 12,000");
         System.out.println("5. Roti Bakar - Rp 10,000");
-        System.out.println("6. Mie Goreng - Rp 18,000");
-        System.out.println("=========================");
-        System.out.println("Silakan pilih menu yang Anda inginkan!");
-    }
-
-    public static void menu(String[] args){
-        menu();
+        System.out.println("6. Mie Goreng - Rp 18.000");
+        System.out.println("===========================");
+        System.out.println("Silakan pilih menu yang Anda inginkan");
     }
     
+    public static void main(String[] args) {
+        Menu();
+    }
 }
