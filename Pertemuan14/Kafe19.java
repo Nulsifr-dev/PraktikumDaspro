f8d05d669c3325326db95e8a47e7ff94e8c0f6d4
Nulsifr-dev <rifqiabdurrahman10@gmail.com>
2025-11-29 21:13:10 +0700

    Kafe19-Pertemuan14-Jobsheet12-P1

--- Pertemuan14/Kafe19.java
+++ Pertemuan14/Kafe19.java
@@ -1,33 +1,20 @@
 package Pertemuan14;
 
 public class Kafe19 {
-
-    public static void Menu(String namaPelanggan, boolean isMember) {
-        System.out.println("Selamat datang, "+namaPelanggan+"!");
-
-        if (isMember) {
-            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian");
-        }
-
-        System.out.println("===== MENU RESTO KAFE =====");
-        System.out.println("1. Kopi Hitam  Rp 15,000");
+    public static void menu() {
+        System.out.println("==== MENU RESTO KAFE ====");
+        System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Capuccino - Rp 20,000");
         System.out.println("3. Latte - Rp 22,000");
         System.out.println("4. Teh Tarik - Rp 12,000");
         System.out.println("5. Roti Bakar - Rp 10,000");
         System.out.println("6. Mie Goreng - Rp 18,000");
-        System.out.println("===========================");
-        System.out.println("Silakan pilih menu yang Anda inginkan.");
-    }
-    public static void main(String[] args) {
-        Menu("Andi", true);
+        System.out.println("=========================");
+        System.out.println("Silakan pilih menu yang Anda inginkan!");
     }
 
-    public static int hitungTotalHarga(int pilihanMenu, int banyakItem){
-        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
-
-        int hargaTotal = hargaItems[pilihanMenu]*banyakItem;
-        return hargaTotal;
+    public static void menu(String[] args){
+        menu();
     }
     
 }
